61757d83885633e43eb1c09cb568a575bec5420f
Seraga Santri <seragasantri@gmail.com>
2025-02-23 10:10:09 +0700

    membuat gambar bentuk lingkaran

--- lib/widget/sample_images.dart
+++ lib/widget/sample_images.dart
@@ -15,21 +15,56 @@ class SampleImages extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Image.network('https://picsum.photos/200/150'), //dari internet
-          Container(
-            margin: const EdgeInsets.all(10),
-            height: 400,
-            width: 400,
-            decoration: BoxDecoration(border: Border.all()),
+          //membuat gambar bentuk lingkaran
+          Image.asset('assets/images/sample_images.jpg'),
+          const CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/images/sample_images.jpg'),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(50),
             child: Image.asset(
-              'assets/images/sample_images.jpg', //dari local
-              alignment: Alignment.centerRight,
-              color: Colors.black,
-              colorBlendMode: BlendMode.softLight,
+              'assets/images/sample_images.jpg',
+              height: 100,
+              width: 100,
               fit: BoxFit.fill,
-              // repeat: ImageRepeat.repeatY,
             ),
           ),
+          ClipOval(
+            child: Image.asset(
+              'assets/images/sample_images.jpg',
+              height: 100,
+              width: 100,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/images/sample_images.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+
+          // Image.network('https://picsum.photos/200/150'), //dari internet
+          // Container(
+          //   margin: const EdgeInsets.all(10),
+          //   height: 400,
+          //   width: 400,
+          //   decoration: BoxDecoration(border: Border.all()),
+          //   child: Image.asset(
+          //     'assets/images/sample_images.jpg', //dari local
+          //     alignment: Alignment.centerRight,
+          //     color: Colors.black,
+          //     colorBlendMode: BlendMode.softLight,
+          //     fit: BoxFit.fill,
+          //     // repeat: ImageRepeat.repeatY,
+          //   ),
+          // ),
         ],
       ),
     );
