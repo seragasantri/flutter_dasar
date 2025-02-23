87be7f55cfcfdf186fe8475b6a6c9a0e63bfe22a
Seraga Santri <seragasantri@gmail.com>
2025-02-23 15:38:05 +0700

    Widget Bottom Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:belajar_widget/widget/sample_bottom_navbar.dart';
 import 'package:belajar_widget/widget/sample_container.dart';
 import 'package:belajar_widget/widget/sample_images.dart';
 import 'package:belajar_widget/widget/sample_listview.dart';
+import 'package:belajar_widget/widget/sample_listview_horizontal.dart';
 import 'package:belajar_widget/widget/sample_text.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
